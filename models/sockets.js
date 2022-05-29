7e972a5c56a7f43f3e715436ae81f2f3d56c8ca0
Sebastian Carrillo <scarrillo@lya-electronic.com>
2022-05-28 22:29:58 -0500

    Socket events

--- models/sockets.js
+++ models/sockets.js
@@ -1,15 +1,17 @@
+const BandList = require("./band-list");
+
 class Sockets {
   constructor(io) {
     this.io = io;
+    this.bandList = new BandList();
     this.socketEvents();
   }
 
   socketEvents() {
     this.io.on("connection", (socket) => {
-      socket.on("message-to-server", (data) => {
-        console.log(data);
-        this.io.emit("message-from-server", data);
-      });
+      console.log("Cliente conectado!");
+      //Emitir al cliente conectado, todas las bandas actuales
+      socket.emit("current-bands", this.bandList.getBands());
     });
   }
 }
